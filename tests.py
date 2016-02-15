f6bdf24e526a99f5099a31849bc7a811fc7160ef
Czyryca <k.czyryca@gmail.com>
2016-02-15 16:41:57 -0500

    Add testing for functions in library

---
+++ tests.py
@@ -0,0 +1,75 @@
+import unittest
+from library import *
+
+class TestCoreFunctions(unittest.TestCase):
+
+    def test_getDate(self):
+        print "Today's date is "+getDate()
+
+    def test_compareDict(self):
+        snapshot1 = {'alice':'human','bob':'human',
+                     'carol':'human','dave':'human'}
+        snapshot2 = {'alice':'zombie','bob':'human',
+                     'carol':'human','dave':'human'}
+        snapshot3 = {'alice':'zombie','bob':'zombie',
+                     'carol':'human','dave':'OZ'}
+        snapshot4 = {'alice':'zombie','bob':'zombie',
+                     'carol':'zombie','dave':'OZ'}
+
+        
+        str_result,humans,zombies = compareDict(snapshot1,snapshot2)
+        self.assertEqual(str_result,'Dying: alice')
+        self.assertEqual(humans,3)
+        self.assertEqual(zombies,1)
+
+        #no change
+        str_result,humans,zombies = compareDict(snapshot2,snapshot2)
+        self.assertEqual(str_result,'')
+        self.assertEqual(humans,3)
+        self.assertEqual(zombies,1)
+
+        str_result,humans,zombies = compareDict(snapshot2,snapshot3)
+        self.assertEqual(str_result,'Dying: bob Revealed OZ: dave')
+        self.assertEqual(humans,1)
+        self.assertEqual(zombies,3)
+
+        str_result,humans,zombies = compareDict(snapshot3,snapshot4)
+        self.assertEqual(str_result,'Dying: carol')
+        self.assertEqual(humans,0)
+        self.assertEqual(zombies,4)
+        
+    def test_compareDictStrange(self):
+        snapshot1 = {'alice':'zombie','bob':'human',
+                     'carol':'human','dave':'human'}
+        snapshot2 = {'alice':'human','bob':'human',
+                     'carol':'human','dave':'human'}
+        snapshot3 = {'alice':'human','bob':'human',
+                     'carol':'human','dave':'human',
+                     'eve':'human'}
+        snapshot4 = {'alice':'human','bob':'human',
+                     'carol':'human','dave':'human',
+                     'eve':'zombie'}
+
+        #test revive
+        str_result,humans,zombies = compareDict(snapshot1,snapshot2)
+        self.assertEqual(str_result,'Revived: alice')
+        self.assertEqual(humans,4)
+        self.assertEqual(zombies,0)
+
+        #test join as human
+        str_result,humans,zombies = compareDict(snapshot2,snapshot3)
+        self.assertEqual(str_result,'Joining: eve')
+        self.assertEqual(humans,5)
+        self.assertEqual(zombies,0)
+        #test join as zombie
+        str_result,humans,zombies = compareDict(snapshot2,snapshot4)
+        self.assertEqual(str_result,'Joining: eve')
+        self.assertEqual(humans,4)
+        self.assertEqual(zombies,1)
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
