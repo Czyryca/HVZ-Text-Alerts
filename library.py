2df5dc4105b272f3f2fa5f29da1fe852f28f166d
Czyryca <k.czyryca@gmail.com>
2016-08-18 21:17:33 -0400

    Removed email support.

--- library.py
+++ library.py
@@ -19,10 +19,12 @@ def getDate():
 #returns a string summarizing changes,count of humans, count of zombies
 #(including OZ in zombie populations)
 def compareDict(old_dict,new_dict):
-    joining = []
+    joining_human = []
+    joining_zombie = []
     dying = []
     revived = []
     revealed_OZs = []
+    removed = []
     human_count = 0
     zombie_count = 0
 
@@ -35,7 +37,13 @@ def compareDict(old_dict,new_dict):
             raise ValueError('Unknown status of player '+player+': '+new_dict[player])
 
         if (player not in old_dict.keys()):#new player joining
-            joining.append(player)
+            if new_dict[player] == 'human':
+                joining_human.append(player)
+            elif new_dict[player] == 'zombie' or new_dict[player] == 'OZ':
+                joining_zombie.append(player)
+            else:
+                print 'strange status of player '+player+': '+new_dict[player]
+
         elif new_dict[player] == old_dict[player]:#no change, most common case
             continue
         elif (new_dict[player]=='zombie' and old_dict[player]=='human'):#ded 
@@ -47,79 +55,24 @@ def compareDict(old_dict,new_dict):
         else:
             print 'finish compare dict' #its 3 AM TODO
 
+    for player in old_dict:
+        if player not in new_dict:
+            removed.append(player)
+
     #return the results of the diff
     result = ''
-    if (len(joining)>0):
-        result += 'Joining: '+(','.join(joining))+' '
+    if (len(joining_human)>0):
+        result += 'Joining as human: '+(', '.join(joining_human))+' '
+    if (len(joining_zombie)>0):
+        result += 'Joining as zombie: '+(', '.join(joining_zombie))+' '
     if (len(dying)>0):
-        result += 'Dying: '+(','.join(dying))+' '
+        result += 'Dying: '+(', '.join(dying))+' '
     if (len(revived)>0):
-        result += 'Revived: '+(','.join(revived))+' '
+        result += 'Revived: '+(', '.join(revived))+' '
     if (len(revealed_OZs)>0):
-        result += 'Revealed OZ: '+(','.join(revealed_OZs))+' '
+        result += 'Revealed OZ: '+(', '.join(revealed_OZs))+' '
+    if (len(removed)>0):
+        result += 'Removed: '+(', '.join(removed))+' '
+    
     return result.strip(), human_count, zombie_count
-
-
-#TODO: untested, but does this need to be tested?
-#If it breaks, I'll know when I launch the script. Can't fail later.
-#prepare to send texts through emails
-#Currently hardcoded to log into umbchvzdeath@gmail.com
-#returns the smtp server
-def setUpEmail():
-    server = smtplib.SMTP("smtp.gmail.com",587)
-    server.starttls()
-    server.login('umbchvzdeath@gmail.com',password)
-    return server
-
-
-
-
-
-
-
-#TODO: depreciated. 
-#assumes connection is set up
-def sendMessage(recipient,deaths,status,change):
-    #first, prepare the message
-    print change.strip()
-    if deaths > 1:
-        msg = str(deaths)+" humans died! "+str(change)
-    elif deaths == 1:
-        msg = str(deaths)+" human died! "+str(change)
-    else:
-        print "strange # of deaths: "+str(deaths)
-        msg = str(deaths)+"? humans died! "+str(change)
-    print "change: "+str(change)
-    print "sending message: "+str(msg)
-
-        
-    #then send it
-    try:
-        server = setUpEmail()
-        server.sendmail('umbchvzdeath@gmail.com',recipient,str(msg))
-    except Exception:
-        server = setUpEmail()
-        try: #this fails randomly sometimes. My apartment has sketchy internet
-            sleep(10)
-            print 'Trying once more to send to '+str(recipient)
-            server.sendmail('umbchvzdeath@gmail.com',recipient,msg)
-            print 'And succeeded.'
-        except Exception:
-            print "But still couldn't"
-
-
-
-#TODO: untested
-#args: int deaths -- the number of people who've died since last update
-#
-def respondToDeaths(deaths,status,change):
-    if deaths > 0: #DED 
-        print str(deaths)+" deaths!"
-        for number in mailing_list:
-            sendMessage(number,deaths,status,change)
-    elif deaths < 0: #explains 'births', either players joining or initializing
-        print 'There are now '+str(new_human_count)+' players.'
-
-
-
 
