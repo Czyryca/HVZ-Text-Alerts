cdf6facc278c602e8eeca55491e8c1e3d3001660
Czyryca <k.czyryca@gmail.com>
2017-02-27 21:35:26 -0500

    Added a delay to messages sent.

--- monitor.py
+++ monitor.py
@@ -5,6 +5,7 @@ then posts changes to a specified GroupMe chat.
 Also logs stats at stats.txt.
 """
 
+from __future__ import print_function
 from urllib2 import urlopen
 from sys import argv
 from time import sleep
@@ -14,10 +15,6 @@ from parser import parser
 from HTMLParser import HTMLParser
 from library import *
 from os import system
-
-def main():
-    first_run = True
-
 
 def main():
     first_run = True
@@ -34,19 +31,24 @@ def main():
         my_parser.feed(str(site))
         new_players = my_parser.getPlayers()
 
+        if not(new_players): #if dict is empty
+            print("Didn't find any players")
+            
+
         #if it's not the first time, check for deaths
         #if it's the first time through the loop, initialize old_human_count
         
         change,humans,zombies = compareDict(old_players,new_players)
         
         if change and not first_run:
-            print change
+            print(change)
             stats = 'at '+getDate()+': '+str(humans)+' Humans, and '+str(zombies)+' Zombies\n'
             f.write(stats) 
-            print stats 
+            print(stats)
             #send message in groupme
             message=change+' -- Humans: '+str(humans)+' Zombies: '+str(zombies)
-            #command = "curl -d '{\"text\" : \"" + message + "\", \"bot_id\" : \"f9d93ab901d96ee9532da9142c\"}' https://api.groupme.com/v3/bots/post"
+            command = "curl -d '{\"text\" : \"" + message + "\", \"bot_id\" : \"f9d93ab901d96ee9532da9142c\"}' https://api.groupme.com/v3/bots/post"
+            command += ' | at now +1 hour '
             system(command)
         else:
             first_run = False
